92e47b9b4ba44392fbbc0d858be56e94b4a23601
Jared Hatfield <jjhatf02@gmail.com>
2024-08-25 20:52:13 -0400

    Allowing all ingress traffic from ALL

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "google_cloud_run_v2_service" "serviceauthcentral-token" {
   project  = var.project_id
   location = each.value
   name     = "${var.name}-token-${each.value}"
-  ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+  ingress  = "INGRESS_TRAFFIC_ALL"
 
   template {
     service_account = google_service_account.cloud_run_sa.email
